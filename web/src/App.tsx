c9e80ce5bec674ee4d2727a03d5b1d9ce126e74c
Sean <toranos@gmail.com>
2018-09-06 22:10:54 +1000

    Add storybook and material-ui

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,22 +1,24 @@
-import * as React from 'react';
-import './App.css';
+import { createMuiTheme, MuiThemeProvider } from "@material-ui/core/styles";
+import * as React from "react";
+import "./App.css";
+import Nav from './components/common/Nav';
 
-import logo from './logo.svg';
-
-class App extends React.Component {
-  public render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
-      </div>
-    );
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: "#b71c1c"
+    },
+    type: "dark"
   }
-}
+});
+
+const App: React.SFC<{}> = () => (
+  <MuiThemeProvider theme={theme}>
+    <Nav/>
+    <p className="App-intro">
+      To get started, edit <code>src/App.tsx</code> and save to reload.
+    </p>
+  </MuiThemeProvider>
+);
 
 export default App;
