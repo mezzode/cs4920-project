bc7004a1609677ed4192cc5eaa5962ca699f7e5c
Sean <toranos@gmail.com>
2018-09-07 12:30:58 +1000

    Add not found route

---
+++ web/src/views/NotFound.tsx
@@ -0,0 +1,25 @@
+import { Typography, WithStyles } from "@material-ui/core";
+import { createStyles, withStyles } from "@material-ui/core/styles";
+import * as React from "react";
+import Nav from "../components/common/Nav";
+
+const styles = createStyles({
+    content: {
+        display: "flex",
+        justifyContent: "space-around",
+
+    }
+});
+
+interface IProps extends WithStyles<typeof styles> { }
+
+const NotFound: React.SFC<IProps> = ({ classes }) => (
+    <React.Fragment>
+        <Nav/>
+        <div className={classes.content}>
+            <Typography variant="display3">404</Typography>
+        </div>
+    </React.Fragment>
+);
+
+export default withStyles(styles)(NotFound);
