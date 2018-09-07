facb4a04426ee4002e7b4152bc32bcfdbb189003
Sean <toranos@gmail.com>
2018-09-07 11:37:18 +1000

    Add landing

---
+++ web/src/views/Landing.tsx
@@ -0,0 +1,29 @@
+import { createStyles, StyleRulesCallback, Theme, Typography, WithStyles, withStyles } from "@material-ui/core";
+import * as React from "react";
+import Nav from "../components/common/Nav";
+
+const styles: StyleRulesCallback = (theme: Theme) => createStyles({
+    "@global": {
+        body: {
+            backgroundColor: theme.palette.primary.dark
+        }
+    },
+    content: {
+        display: "flex",
+        justifyContent: "space-around",
+        
+    }
+})
+
+interface IProps extends WithStyles<typeof styles> { }
+
+const Landing: React.SFC<IProps> = ({classes}) => (
+    <React.Fragment>
+        <Nav transparent={true} />
+        <div className={classes.content}>
+            <Typography variant="display3" color="textPrimary">Welcome to medialog</Typography>
+        </div>
+    </React.Fragment>
+);
+
+ export default withStyles(styles)(Landing);
