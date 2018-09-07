dce236fde6c7313a32eaac3ee699648022282d23
Sean <toranos@gmail.com>
2018-09-07 12:30:58 +1000

    Add not found route

--- web/src/views/index.tsx
+++ web/src/views/index.tsx
@@ -1,15 +1,41 @@
 import { CssBaseline } from "@material-ui/core";
 import * as React from "react";
-import { Route, Switch } from "react-router";
+import { connect, MapStateToProps } from "react-redux";
+import { Route, RouteComponentProps, Switch, withRouter } from "react-router";
+import { IState } from "../reducers";
 import Landing from "./Landing";
+import NotFound from "./NotFound";
 
-const View = () => (
-    <React.Fragment>
-        <CssBaseline />
+const View: React.SFC<IProps> = ({auth}) => {
+    const authRoutes = (
+        <Switch>
+            <Route component={NotFound} />
+        </Switch>
+    )
+    const unauthRoutes = (
         <Switch>
             <Route exact={true} path="/" component={Landing} />
+            <Route component={NotFound} />
         </Switch>
-    </React.Fragment>
-)
+    );
+    return (
+        <React.Fragment>
+            <CssBaseline />
+            {auth ? authRoutes : unauthRoutes}
+        </React.Fragment>
+    );
+}
 
-export default View;
+interface IStateProps {
+    auth: boolean;
+}
+
+interface IOwnProps extends RouteComponentProps<{}> { }
+
+type IProps = IStateProps;
+
+const mapStateToProps: MapStateToProps<IStateProps, IOwnProps, IState> = state => ({
+    auth: state.user.displayName !== null
+})
+
+export default withRouter(connect(mapStateToProps)(View));
