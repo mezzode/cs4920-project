9c101ff0eac9c4f3789f1d62899b8266b25d73ac
Sean <toranos@gmail.com>
2018-09-07 17:49:00 +1000

    Add transparent story

--- web/stories/index.stories.tsx
+++ web/stories/index.stories.tsx
@@ -9,9 +9,7 @@ import { RawNav, UnconnectedNav } from "../src/components/common/Nav";
 storiesOf("Nav", module)
     .addDecorator(story => (
         <MemoryRouter>
-            <MuiThemeProvider theme={theme}>
-                {story()}
-            </MuiThemeProvider>
+            <MuiThemeProvider theme={theme}>{story()}</MuiThemeProvider>
         </MemoryRouter>
     ))
     .add(
@@ -38,4 +36,38 @@ storiesOf("Nav", module)
                 ~~~
             `
         })(() => <UnconnectedNav user={{ displayName: null }} />)
+    )
+    .add(
+        "Logged Out (transparent)",
+        withInfo({
+            header: false,
+            inline: true,
+            // show props of the actual component instead of the wrapper
+            propTables: [RawNav],
+            propTablesExclude: [UnconnectedNav],
+            // hiding generated source since wrong name and would not use directly
+            source: false,
+            text: `
+                Styles are provided to this component by \`withStyles\` from \`@material-ui/core\`,
+                which also adds the \`classes\`, \`innerRef\`, and \`theme\` props.
+
+                Technically this story uses \`RawNav\` but this component should never be used directly.
+                Instead use \`Nav\` which connects it to the app's global redux state to inject \`user\`.
+                
+                ## Code
+
+                ~~~tsx
+                <div style={{backgroundColor: 'black'}}>
+                    <Nav transparent={true} />
+                </div>
+                ~~~
+            `
+        })(() => (
+            <div style={{ backgroundColor: "black" }}>
+                <UnconnectedNav
+                    user={{ displayName: null }}
+                    transparent={true}
+                />
+            </div>
+        ))
     );
