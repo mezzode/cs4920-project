087cab3152a16c870d72f91265067abcb196cd09
Sean <toranos@gmail.com>
2018-09-18 23:22:56 +1000

    Fix db connection details

--- server/src/db.ts
+++ server/src/db.ts
@@ -8,7 +8,11 @@ const pgp: IMain = pgPromise({});
 //     host: 'postgres',
 //     port: 5433
 // };
-const cn = 'postgres://postgres@localhost:5433/appdb';
+// https://docs.docker.com/compose/networking/
+// containers join their network using their name. our db container is named "postgres"
+// containers on the network connect to each other using the container port, not the host port
+// the host port is only used for accessing the container from the host
+const cn = 'postgres://postgres@postgres:5432/appdb';
 
 export const db: IDatabase<any> = pgp(cn);
 
