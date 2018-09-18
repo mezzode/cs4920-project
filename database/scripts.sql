eddf0d4a0f3103e86673e3e8a2286278b720bdda
Jeremy Fu <jfu@atlassian.com>
2018-09-18 20:58:48 +1000

    login server initial

--- database/scripts.sql
+++ database/scripts.sql
@@ -6,15 +6,15 @@ CREATE TABLE users (
     image bytea
 );
 CREATE TABLE list (
-    id int primary key not null
+    id serial primary key not null
 );
 CREATE TABLE media (
-    id int primary key not null,
-    api_id int not null
+    id serial primary key not null,
+    api_id text not null
 );
 CREATE TYPE status AS ENUM ('progress', 'complete');
 CREATE TABLE entry (
-    id int primary key not null,
+    id serial primary key not null,
     media_id int not null,
     user_id int not null,
     status status,
@@ -23,8 +23,22 @@ CREATE TABLE entry (
     foreign key (user_id) references users(id) 
 );
 
--- Insert data
+-- Users
 INSERT INTO users VALUES (DEFAULT, 'jfu', 'foobar');
+INSERT INTO users VALUES (DEFAULT, 'user1', 'pass1');
+INSERT INTO users VALUES (DEFAULT, 'user2', 'pass2');
+INSERT INTO users VALUES (DEFAULT, 'user3', 'pass3');
+INSERT INTO users VALUES (DEFAULT, 'user4', 'pass4');
+
+-- Media
+INSERT INTO media VALUES (DEFAULT, '12345');
+INSERT INTO media VALUES (DEFAULT, '67890');
+
+-- Entry
+INSERT INTO entry VALUES (DEFAULT, 1, 1, 'progress', 9);
+INSERT INTO entry VALUES (DEFAULT, 2, 1, 'complete', 4);
+INSERT INTO entry VALUES (DEFAULT, 1, 2, 'progress', 5);
+INSERT INTO entry VALUES (DEFAULT, 2, 3, 'complete', 1);
 
 -- SELECT * FROM users;
 
