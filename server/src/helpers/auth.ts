97ca087d9785a70b762763f1c5fdaf6a2e0b46d5
Jeremy <imjeremyhi@gmail.com>
2018-09-24 22:36:53 +1000

    profile and login pages initial (#10)
    * profile and login pages initial
    
    * basic pages
    
    * fix code style

---
+++ server/src/helpers/auth.ts
@@ -0,0 +1,53 @@
+import { RequestHandler } from 'express';
+import * as passport from 'passport';
+import { Strategy as LocalStrategy } from 'passport-local';
+import { IDatabase } from 'pg-promise';
+
+export { passport };
+
+export const isLoggedIn: RequestHandler = (req, res, next) => {
+    if (req.isAuthenticated()) {
+        return next();
+    }
+    return res.redirect('/login');
+};
+
+export const setupAuth = (db: IDatabase<any>) => {
+    passport.use(
+        new LocalStrategy(async (username, password, done) => {
+            try {
+                const user = await db.oneOrNone({
+                    text: 'select * from users where username = $1',
+                    values: [username],
+                });
+                console.log(JSON.stringify(user));
+                if (!user) {
+                    return done(null, false, {
+                        message: 'Incorrect username.',
+                    });
+                }
+                if (user.password !== password) {
+                    return done(null, false, {
+                        message: 'Incorrect password.',
+                    });
+                }
+                return done(null, user);
+            } catch (e) {
+                return done(e);
+            }
+        }),
+    );
+
+    passport.serializeUser((user, done) => {
+        done(null, user);
+        // done(null, user.id);
+    });
+
+    passport.deserializeUser((user, done) => {
+        // function (id,done)
+        done(null, user);
+        // User.findById(id, function (err, user) {
+        //     done(err, user);
+        // });
+    });
+};
---
+++ server/src/routes.ts
@@ -0,0 +1,42 @@
+import * as express from 'express';
+import * as path from 'path';
+
+import { isLoggedIn, passport } from './helpers/auth';
+import { db } from './helpers/db';
+
+export const router = express.Router();
+
+router.get('/healthcheck/db', async (req: any, res: any) => {
+    try {
+        const data = await db.query('select * from users;');
+        console.log(JSON.stringify(data));
+        res.send(JSON.stringify(data));
+    } catch (e) {
+        res.send(`failed to retrieve data. ${e}`);
+    }
+});
+
+router.get('/dashboard', isLoggedIn, (req: any, res: any) => {
+    console.log('in dashboard');
+    res.sendFile(path.resolve(`${__dirname}/../../web/build/index.html`));
+});
+
+router.get('/profile', isLoggedIn, (req: any, res: any) => {
+    console.log('in profile');
+    console.log(JSON.stringify(req.user));
+    const { id, username, image } = req.user;
+    res.send(`id: ${id}, username: ${username}, image: ${image}`);
+    // res.sendFile(path.resolve(`${__dirname}/../../web/build/profile.html`));
+});
+
+router
+    .route('/login')
+    .get((req: any, res: any) => {
+        res.sendFile(path.resolve(`${__dirname}/../../web/build/login.html`));
+    })
+    .post(passport.authenticate('local'), (req: any, res: any) => {
+        console.log(JSON.stringify(req.user));
+        res.redirect('/dashboard');
+    });
+
+// router.get('/api/getUsername', (req: any, res: any) => res.send({ username: os.userInfo().username }));
