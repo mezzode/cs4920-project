a597d87f0cf6686bb52fabb21505e6d51c38db07
Nelch <nelsonchen761@gmail.com>
2018-09-28 02:52:46 +1000

    Finished the default search page

--- web/src/views/Search.tsx
+++ web/src/views/Search.tsx
@@ -1,5 +1,8 @@
-import { Theme, Typography } from '@material-ui/core';
+import { Theme, WithStyles } from '@material-ui/core';
+import { Grid, IconButton } from '@material-ui/core';
+import Input from '@material-ui/core/Input';
 import { createStyles, withStyles } from '@material-ui/core/styles';
+import SearchIcon from '@material-ui/icons/Search';
 import * as React from 'react';
 import { connect } from 'react-redux';
 import { Nav } from '../components/common/Nav';
@@ -7,6 +10,12 @@ import { Nav } from '../components/common/Nav';
 // TODO: fiddle with styling
 const styles = (theme: Theme) =>
     createStyles({
+        inputInput: {
+            fontSize: 34,
+        },
+        inputRoot: {
+            width: '75%',
+        },
         layout: {
             marginLeft: theme.spacing.unit * 3,
             marginRight: theme.spacing.unit * 3,
@@ -14,8 +23,24 @@ const styles = (theme: Theme) =>
             width: 'auto',
         },
         paper: {},
+        search: {
+            margin: 'auto',
+            position: 'absolute',
+            right: '0%',
+            top: '50%',
+        },
+        searchButton: {
+            float: 'left',
+            height: 60,
+            width: 60,
+        },
+        searchIcon: {
+            height: 60,
+            width: 60,
+        },
     });
 
+interface Props extends WithStyles<typeof styles> {}
 /*
 interface Props {}
 
@@ -31,19 +56,55 @@ export const Search = withStyles(styles)(SearchResults);
 
 export const SearchPage = connect()(
     withStyles(styles)(
-        class extends React.Component {
+        class extends React.Component<Props> {
             public render() {
+                const { classes } = this.props;
                 const urlString = window.location.href;
                 const url = new URL(urlString);
                 const query = url.searchParams.get('q');
+
+                let content = null;
+                if (query === '') {
+                    content = (
+                        <Grid
+                            container={true}
+                            spacing={0}
+                            direction="column"
+                            alignItems="center"
+                            justify="center"
+                            style={{ minHeight: '60vh' }}
+                        >
+                            <form action="/search">
+                                <IconButton
+                                    type="submit"
+                                    classes={{ root: classes.searchButton }}
+                                >
+                                    <SearchIcon
+                                        classes={{ root: classes.searchIcon }}
+                                    />
+                                </IconButton>
+                                <Input
+                                    autoComplete="off"
+                                    name="q"
+                                    type="text"
+                                    id="q"
+                                    placeholder="Search"
+                                    classes={{
+                                        input: classes.inputInput,
+                                        root: classes.inputRoot,
+                                    }}
+                                />
+                            </form>
+                        </Grid>
+                    );
+                } else {
+                    content = <div>{query}</div>;
+                }
                 return (
                     <>
                         <Nav />
-                        <Typography variant="display3">
-                            SEARCH RESULTS
-                        </Typography>
                         HI what is up my dudes
-                        <div>{query}</div>
+                        {content}
                     </>
                 );
             }
