a0476760bcfda672ec4c1dec40fa9e68b5c55394
Jeremy Fu <jfu@atlassian.com>
2018-09-27 20:07:15 +1000

    move env to docker compose and change port

--- server/src/helpers/db.ts
+++ server/src/helpers/db.ts
@@ -2,15 +2,9 @@ import { IDatabase, IMain } from 'pg-promise';
 import * as pgPromise from 'pg-promise';
 
 const pgp: IMain = pgPromise({});
-// const cn = {
-//     database: 'appdb',
-//     user: 'postgres',
-//     host: 'postgres',
-//     port: 5433
-// };
 // https://docs.docker.com/compose/networking/
 // containers join their network using their name. our db container is named "postgres"
 // containers on the network connect to each other using the container port, not the host port
 // the host port is only used for accessing the container from the host
-const cn = `postgres://postgres@${process.env.HOST}:5433/appdb`;
+const cn = `postgres://postgres@${process.env.HOST}/appdb`;
 export const db: IDatabase<any> = pgp(cn);
