24db834ecf05ee6e2b773c4e700c7b774c71846b
Jeremy Fu <jfu@atlassian.com>
2018-10-12 18:39:30 +1100

    resolve merge conflict

--- database/scripts/create.sql
+++ database/scripts/create.sql
@@ -1,37 +1,3 @@
-<<<<<<< HEAD
--- Create schema
-CREATE TABLE users
-(
-    id serial primary key not null,
-    username text not null,
-    password text not null,
-    image bytea
-);
-CREATE TABLE list
-(
-    id serial primary key not null
-);
-CREATE TYPE mediaType as ENUM
-('game', 'anime', 'movie', 'tv');
-CREATE TABLE media
-(
-    id serial primary key not null,
-    mediaType mediaType,
-    api_id text not null
-);
-CREATE TYPE status AS ENUM
-('progress', 'complete');
-CREATE TABLE entry
-(
-    id serial primary key not null,
-    media_id int not null,
-    user_id int not null,
-    status status,
-    rating int,
-    foreign key (media_id) references media(id),
-    foreign key (user_id) references users(id)
-);
-=======
 -- Create schema
 CREATE TABLE users
 (
@@ -71,4 +37,3 @@ CREATE TABLE entry
     started text,
     finished text
 );
->>>>>>> 9459a4c421f0866f284f8463b358bad81ae4374a
