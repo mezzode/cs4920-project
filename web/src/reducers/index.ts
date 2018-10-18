3b6b7c7a269ad92b365ddbfd94b4291372f563fb
Jeremy Fu <jfu@atlassian.com>
2018-10-18 17:34:17 +1100

    merge

--- web/src/reducers/index.ts
+++ web/src/reducers/index.ts
@@ -1,35 +1,13 @@
 import { combineReducers, Reducer } from 'redux';
-<<<<<<< HEAD
-import { lists, ListsState } from './displayedLists';
-import { entryEditor, EntryEditorState } from './entryEditor';
-import { flashMessage, FlashMessageState } from './flashMessage';
-import { media, MediaState } from './media';
-import { search, SearchState } from './search';
-import { user, UserState } from './user';
-
-export interface State {
-    lists: ListsState;
-    entryEditor: EntryEditorState;
-    flashMessage: FlashMessageState;
-    media: MediaState;
-    search: SearchState;
-    user: UserState;
-}
-=======
 import { flashMessage } from './flashMessage';
+import { media } from './media';
 import { modals } from './modals';
 import { user } from './user';
->>>>>>> 3a738746cbb0906da1cdc8beca549945ff139196
 
 export const rootReducer = combineReducers({
     flashMessage,
-<<<<<<< HEAD
-    lists,
     media,
-    search,
-=======
     modals,
->>>>>>> 3a738746cbb0906da1cdc8beca549945ff139196
     user,
 });
 
