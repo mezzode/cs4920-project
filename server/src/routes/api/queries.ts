de51fa0f8f39e0521188e920895e56f6786d5607
Sean <toranos@gmail.com>
2018-10-21 17:22:38 +1100

    Fix IGDB limit by manually expanding fields

--- server/src/routes/api/queries.ts
+++ server/src/routes/api/queries.ts
@@ -1,4 +1,4 @@
-exports.entryA = `
+export const entryA = `
 query ($id: Int) {
   Media(id: $id, type: ANIME) {
     id
@@ -28,7 +28,7 @@ query ($id: Int) {
 }
 `;
 
-exports.searchA = `query ($page: Int, $perPage: Int, $search: String) {
+export const searchA = `query ($page: Int, $perPage: Int, $search: String) {
   Page (page: $page, perPage: $perPage) {
     pageInfo {
       total
@@ -52,39 +52,11 @@ exports.searchA = `query ($page: Int, $perPage: Int, $search: String) {
 }
 `;
 
-exports.gameFetchQuery = function (id) {
-  return {
-    ids: [id],
-    fields: [
-      'id',
-      'name',
-      'summary',
-      'category',
-      'genres.name',
-      'themes.name',
-      'publishers.name',
-      'developers.name',
-      'first_release_date',
-      'status',
-      'cover',
-    ],
-    expand: [
-      'genres',
-      'themes',
-      'publishers',
-      'developers',
-    ],
-
-  };
-}
-
-exports.gameSearchQuery = function (name, page) {
-  return {
+export const gameSearchQuery = (name: string, page: number) => ({
+    // order: 'name:asc',
     fields: ['id', 'name', 'summary', 'cover'],
     limit: '20',
     offset: (page - 1) * 20,
-    //order: 'name:asc',
     scroll: 1,
     search: name,
-  };
-}
+});
